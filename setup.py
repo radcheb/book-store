0801243c8c230f8239a90ab5ab9dd56febc6ca59
Radhwane Chebaane <radcheb@gmail.com>
2019-10-27 23:05:44 +0100

    setup package info

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='best_book_store',
+      version='0.0.2',
+      description='The best book store management system',
+      url='https://github.com/radcheb/book-store',
+      author='radcheb',
+      author_email='radcheb@example.com',
+      license='MIT',
+      packages=['best_book_store'],
+      zip_safe=False)
